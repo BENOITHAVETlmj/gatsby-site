42a089cc77dfc39d47d2e1aaa694a9cf7c75009d
BENOIT-HAVET LEE <myungjae84@.com>
2020-05-23 17:04:55 +0200

    add ts plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hello world</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Let's build our first site</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to next page</Link>
   </Layout>
 )
 
